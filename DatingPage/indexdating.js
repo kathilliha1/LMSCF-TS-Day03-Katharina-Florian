5dbd8f7eb41dc33b788c5a57cc500e2a99cb4a13
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-20 14:40:24 +0100

    test

--- DatingPage/indexdating.js
+++ DatingPage/indexdating.js
@@ -20,9 +20,19 @@ var Datinglike = /** @class */ (function () {
         this.favourite = f;
     }
     Datinglike.prototype.datingdown = function () {
-        return "\n\t\t<div class= col-sm-4 bg-dark border>\n\t\t<img src=\"" + this.image + "\" class=\"rounded \" alt=\"Cinque Terre\">\n\n\t<p class=\" row justify-content-start\">Name: " + this.name + "</p>\n\t<p class=\" row justify-content-start\">Age: " + this.age + "</p>\n\t<p class=\" row justify-content-start\">Location: " + this.location + "</p>\n\t<p class=\" row justify-content-start\">Hobbies: " + this.hobbies + "</p>\n\t<p class=\" row justify-content-start\">Favorites: " + this.favourite + "</p>\n\t\n\t\n</div>\n\t\t";
+        return "\n\t\t<div class= col-sm-3 bg-dark border>\n\t\t<img src=\"" + this.image + "\" class=\" rounded pic\" alt=\"Cinque Terre\">\n\n\t<p class=\" row justify-content-start\">Name: " + this.name + "</p>\n\t<p class=\" row justify-content-start\">Age: " + this.age + "</p>\n\t<p class=\" row justify-content-start\">Location: " + this.location + "</p>\n\t<p class=\" row justify-content-start\">Hobbies: " + this.hobbies + "</p>\n\t<p class=\" row justify-content-start\">Favorites: " + this.favourite + "</p>\n\t\n\t\n</div>\n\t\t";
     };
     return Datinglike;
 }());
-var pleaseDateMe = new Datinglike("../Images/DarthMaul.jpg", "Karl", 49, "Vienna", "sometimes", "nudelmonster");
-document.getElementsByClassName("upp")[0].innerHTML = (pleaseDateMe.datingdown());
+var pleaseDateOne = new Datinglike("../Images/DarthMaul.jpg", "Darth Moul", 49, "Vienna", "sometimes", "nudelmonster");
+var pleaseDatetwo = new Datinglike("../Images/DarthVader.png", "Darth Vader", 55, "Lang Enzersdorf", "Schnorcheln", "eis essen");
+var pleaseDateThree = new Datinglike("../Images/JarJar.jpg", "Jar Jar Bings", 14, "star wars", "comedy club", "blabla");
+var pleaseDateFour = new Datinglike("../Images/JynErso.jpg", "Jyn Erso", 27, "mistelbach", "pläne fladern", "d");
+var pleaseDateFive = new Datinglike("../Images/Padme.jpg", "Padme", 30, "ff", "d", "d");
+var pleaseDateSix = new Datinglike("../Images/PrinzessionLea.jpg", "Prinzession Lea", 40, "ff", "üü", "dd");
+var pleaseDateSeven = new Datinglike("../Images/RaySkywalker.png", "Ray Skywalker", 89, "asdf", "asdf", "sdf");
+var pleaseDateEight = new Datinglike("../Images/Yoda.png", "Joda", 29, "asdfsadf", "asdfdsf", "asdfdsf");
+var downout = [pleaseDateOne, pleaseDatetwo, pleaseDateThree, pleaseDateFour, pleaseDateFive, pleaseDateSix, pleaseDateSeven, pleaseDateEight];
+for (var i = 0; i < downout.length; i++) {
+    document.getElementsByClassName("upp")[0].innerHTML += (downout[i].datingdown());
+}
--- DatingPage/indexdating.ts
+++ DatingPage/indexdating.ts
@@ -49,8 +49,8 @@ class Datinglike{
 
 	datingdown(){
 		return `
-		<div class= col-sm-4 bg-dark border>
-		<img src="${this.image}" class="rounded " alt="Cinque Terre">
+		<div class= col-sm-3 bg-dark border>
+		<img src="${this.image}" class=" rounded pic" alt="Cinque Terre">
 
 	<p class=" row justify-content-start">Name: ${this.name}</p>
 	<p class=" row justify-content-start">Age: ${this.age}</p>
@@ -67,9 +67,32 @@ class Datinglike{
 
 
 
-let pleaseDateMe = new Datinglike("../Images/DarthMaul.jpg", "Karl", 49, "Vienna", "sometimes", "nudelmonster");
+let pleaseDateOne = new Datinglike("../Images/DarthMaul.jpg", "Darth Moul", 49, "Vienna", "sometimes", "nudelmonster");
+
+let pleaseDatetwo = new Datinglike("../Images/DarthVader.png", "Darth Vader", 55, "Lang Enzersdorf", "Schnorcheln", "eis essen");
+
+let pleaseDateThree = new Datinglike("../Images/JarJar.jpg", "Jar Jar Bings", 14, "star wars", "comedy club", "blabla"  )
+
+let pleaseDateFour = new Datinglike("../Images/JynErso.jpg", "Jyn Erso", 27, "mistelbach", "pläne fladern", "d")
+
+let pleaseDateFive = new Datinglike("../Images/Padme.jpg", "Padme", 30, "ff", "d", "d")
+
+let pleaseDateSix = new Datinglike("../Images/PrinzessionLea.jpg", "Prinzession Lea", 40, "ff", "üü", "dd")
+
+let pleaseDateSeven = new Datinglike("../Images/RaySkywalker.png", "Ray Skywalker", 89,  "asdf", "asdf", "sdf")
+
+let pleaseDateEight = new Datinglike("../Images/Yoda.png", "Joda", 29, "asdfsadf", "asdfdsf", "asdfdsf")
+
+
+let downout = [pleaseDateOne, pleaseDatetwo, pleaseDateThree, pleaseDateFour, pleaseDateFive, pleaseDateSix, pleaseDateSeven, pleaseDateEight]
+
+for (let i = 0; i < downout.length; i++){ 
 
 
 
-document.getElementsByClassName("upp")[0].innerHTML = (pleaseDateMe.datingdown());
+document.getElementsByClassName("upp")[0].innerHTML += (downout[i].datingdown());	
+}
+
+
+
 
